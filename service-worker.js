8d91793a38d830744d1232ca1cf32c00b1178055
rafi-windows <mochrafinuroktafian@outlook.com>
2020-07-09 15:41:16 +0700

    sw -> workbox

--- service-worker.js
+++ service-worker.js
@@ -1,67 +1,38 @@
-const CACHE_NAME = "submission-pwa";
-const urlsToCache = [
-    "/",
-    "/manifest.json",
-    "/nav.html",
-    "/index.html",
-    "/team.html",
-    "/pages/home.html",
-    "/pages/about.html",
-    "/pages/contact.html",
-    "/pages/saved.html",
-    "/css/materialize.min.css",
-    "/js/materialize.min.js",
-    "/js/nav.js",
-    "/js/api.js",
-    "/js/idb.js",
-    "/js/db.js",
-    "/img/icon.png",
-    "/img/notif.png"
-];
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js');
 
-self.addEventListener("install", function (event) {
-    event.waitUntil(
-        caches.open(CACHE_NAME).then(function (cache) {
-            return cache.addAll(urlsToCache);
-        })
-    );
+if (workbox) {
+    console.log('Workbox berhasil dimuat');
+} else {
+    console.log('Workbox gagal dimuat');
+}
+
+workbox.precaching.precacheAndRoute([
+    { url: '/', revision: '1' },
+    { url: '/index.html', revision: '1' },
+    { url: '/nav.html', revision: '1' },
+    { url: '/team.html', revision: '1' },
+    { url: '/pages/home.html', revision: '1' },
+    { url: '/pages/saved.html', revision: '1' },
+    { url: '/pages/about.html', revision: '1' },
+    { url: '/pages/contact.html', revision: '1' },
+    { url: '/css/materialize.min.css', revision: '1' },
+    { url: '/js/materialize.min.js', revision: '1' },
+    { url: '/js/nav.js', revision: '1' },
+    { url: '/js/api.js', revision: '1' },
+    { url: '/js/idb.js', revision: '1' },
+    { url: '/js/db.js', revision: '1' },
+    { url: '/img/icon.png', revision: '1' },
+    { url: '/img/notif.png', revision: '1' },
+    { url: '/manifest.json', revision: '1' },
+], {
+    // Ignore all URL parameters.
+    ignoreUrlParametersMatching: [/.*/]
 });
 
-self.addEventListener("fetch", function (event) {
-    const base_url = "https://api.football-data.org/v2/";
-
-    if (event.request.url.indexOf(base_url) > -1) {
-        event.respondWith(
-            caches.open(CACHE_NAME).then(function (cache) {
-                return fetch(event.request).then(function (response) {
-                    cache.put(event.request.url, response.clone());
-                    return response;
-                });
-            })
-        );
-    } else {
-        event.respondWith(
-            caches.match(event.request, { ignoreSearch: true }).then(function (response) {
-                return response || fetch(event.request);
-            })
-        );
-    }
-});
-
-self.addEventListener("activate", function (event) {
-    event.waitUntil(
-        caches.keys().then(function (cacheNames) {
-            return Promise.all(
-                cacheNames.map(function (cacheName) {
-                    if (cacheName != CACHE_NAME) {
-                        console.log(`ServiceWorker: cache ${cacheName} dihapus`);
-                        return caches.delete(cacheName);
-                    }
-                })
-            );
-        })
-    );
-});
+workbox.routing.registerRoute(
+    new RegExp('https://api.football-data.org/v2/'),
+    workbox.strategies.staleWhileRevalidate()
+)
 
 //Response Push Notification
 self.addEventListener('push', function (event) {
